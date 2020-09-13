41f56ee52f8def21647a877cbb945edf8f8e2e0c
unknown <1043536089@qq.com>
2020-09-13 12:10:22 +0800

    init1

--- src/view/yong_hu_guan_li/ban_zhuan_ye_jiao_lain/ban_zhuan_ye_jiao_lian_xiang_qing.vue
+++ src/view/yong_hu_guan_li/ban_zhuan_ye_jiao_lain/ban_zhuan_ye_jiao_lian_xiang_qing.vue
@@ -1,54 +1,122 @@
 <template>
   <div>
-    <Modal v-model="modal2" width="460">
-      <p slot="header" style="color:#f60;text-align:center">
-        <Icon type="ios-information-circle"></Icon>
-        <span>审核退款</span>
+     <Modal v-model="modal2" :maskClosable="false" footer-hide width="750" @on-cancel="Cancel">
+      <p slot="header" style="text-align:center">
+        <span>教练详情</span>
       </p>
       <div>
         <div class="div_content">
-          <span>付款类型：</span>
-          {{'训练营'}}
+          <span>教练ID：</span>
+          <div>{{'1101021987'}}</div>
         </div>
         <div class="div_content">
-          <span>申请时间：</span>
-          {{'yyyy-MM-dd HH:mm:ss'}}
-        </div>
-
-        <div class="div_content">
-          <span>退款名称：：</span>
-          {{'暑期训练营杭州站'}}
+          <span>手机账号：</span>
+          <div>{{'18611587069'}}</div>
         </div>
         <div class="div_content">
-          <span>用户信息：</span>
-          {{'尤硕/18611587069'}}
+          <span>注册时间：</span>
+          <div>{{'yyyy-MM-dd HH:mm:ss'}}</div>
         </div>
         <div class="div_content">
-          <span>支付信息：</span>
-          {{'支付宝/20000000111111112899'}}
+          <span>账号级别：</span>
+          <div>{{'专家账号'}}</div>
         </div>
         <div class="div_content">
-          <span>退款金额：</span>
-          {{3800.00}}
+          <span>账号状态：</span>
+          <div>{{'审核通过'}}</div>
         </div>
         <div class="div_content">
-          <span>订单状态：</span>
-          {{'退款成功'}}
+          <span>审核提交时间：</span>
+          <div>{{'yyyy-MM-dd HH:mm:ss'}}</div>
         </div>
         <div class="div_content">
-          <span>退款操作：</span>操作者姓名
+          <span>教练姓名：</span>
+          <div>{{'尤硕'}}</div>
         </div>
         <div class="div_content">
-          <span>退款操作时间：</span>yyyy-MM-dd HH:mm:ss
+          <span>昵称：</span>
+          <div>{{"尤硕"}}</div>
         </div>
         <div class="div_content">
-          <span>退款结果：</span>
-          退款成功
+          <span>性别：</span>
+          <div>{{"男"}}</div>
         </div>
-        <div class="div_content">备注：</div>
-      </div>
-      <div slot="footer">
-        <!-- <Button type="error" size="large" long :loading="modal_loading" @click="del">Delete</Button> -->
+        <div class="div_content">
+          <span>身份证号：</span>
+          <div>{{"110108198805041111"}}</div>
+        </div>
+        <div class="div_content">
+          <span>教练照：</span>
+          <div>
+            <img src alt />
+          </div>
+        </div>
+        <div class="div_content">
+          <span>头像：</span>
+          <div>
+            <img src alt />
+          </div>
+        </div>
+        <div class="div_content">
+          <span>工作性质：</span>
+          <div>全职</div>
+        </div>
+        <div class="div_content">
+          <span>所在城市：</span>
+          <div>{{"北京市/北京市/海淀区"}}</div>
+        </div>
+        <div class="div_content">
+          <span>所属机构：</span>
+          <div>{{"喜悦-大兴店"}}</div>
+        </div>
+        <div class="div_content">
+          <span>教练简介：</span>
+          <div>{{"教练简介内容教练简介内容教练简介内容教练简介内容"}}</div>
+        </div>
+        <div class="div_content">
+          <span>账户余额(i币)：</span>
+          <div>{{"0"}}</div>
+        </div>
+        <div class="div_content">
+          <span>消费总金额(元)：</span>
+          <div>{{"0"}}</div>
+        </div>
+        <div class="div_content">
+          <span>收益总金额(元)：</span>
+          <div>{{"0"}}</div>
+        </div>
+        <div class="div_content">
+          <span>认证状态:</span>
+          <div>{{"审核通过"}}</div>
+        </div>
+        <div class="div_content">
+          <span>认证提交时间：</span>
+          <div>{{"yyyy-MM-dd HH:mm:ss"}}</div>
+        </div>
+        <div class="div_content">
+          <span>专业项目：</span>
+          <div>{{"花样滑冰"}}</div>
+        </div>
+        <div class="div_content">
+          <span>专业滑冰开始时间：</span>
+          <div>{{"yyyy-MM-dd HH:mm:ss"}}</div>
+        </div>
+        <div class="div_content">
+          <span>运动生涯成绩：</span>
+          <div>{{"运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩"}}</div>
+        </div>
+        <div class="div_content">
+          <span>滑冰教学开始时间：</span>
+          yyyy-MM
+        </div>
+        <div class="div_content">
+          <span>教学特点：</span>
+          <div>{{"运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩"}}</div>
+        </div>
+        <div class="div_content">
+          <span>教学成果：</span>
+          <div>{{"运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩运动生涯成绩"}}</div>
+        </div>
       </div>
     </Modal>
   </div>
@@ -56,6 +124,10 @@
 
 <script>
 export default {
+  props: {
+    coachInfo: Object,
+    onCancel: Function,
+  },
   data() {
     return {
       modal2: true,
@@ -67,6 +139,9 @@ export default {
     };
   },
   methods: {
+     Cancel() {
+      this.onCancel();
+    },
     del() {
       this.modal_loading = true;
       setTimeout(() => {
@@ -80,17 +155,17 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+
 .div_content {
   margin-bottom: 10px;
   display: flex;
   align-items: center;
-}
-.div_content {
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-  .btn {
-    margin-left: 65%;
+  > span {
+    width: 115px;
+    text-align: right;
+  }
+  > div {
+    flex: 1;
   }
 }
 </style>
