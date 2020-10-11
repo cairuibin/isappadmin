6148c4b97ca339a9837c0c5cf6d4d1115b3f87a2
rgb613 <1920559968@qq.com>
2020-10-11 15:48:10 +0800

    [t0]1111111111111111

--- src/view/yun_ying_guan_li/ke_cheng_ding_dan/ke_cheng_ding_dan_lie_biao.vue
+++ src/view/yun_ying_guan_li/ke_cheng_ding_dan/ke_cheng_ding_dan_lie_biao.vue
@@ -1,38 +1,71 @@
 <template>
   <div>
     <Card>
-      <div style="margin-bottom:10px;" class="header_wrap">
+      <div style="margin-bottom: 10px" class="header_wrap">
         <span>订单类型</span>
-        <Select v-model="lei_xing" placeholder="全部类型" style="width:100px">
+        <Select
+          v-model="lei_xing"
+          @on-change="lei_xingchange()"
+          placeholder="全部类型"
+          style="width: 100px"
+        >
           <!-- <Option value="quan_bu_lei_xing">全部类型</Option> -->
-          <Option value="ke_bao">课包</Option>
-          <Option value="qing_xun_ying">青训营</Option>
+          <Option value="1">课包</Option>
+          <Option value="2">青训营</Option>
         </Select>
         <span>订单状态</span>
-        <Select v-model="zhuang_tai" placeholder="全部状态" style="width:100px">
+        <Select
+          v-model="zhuang_tai"
+          placeholder="全部状态"
+          style="width: 100px"
+          @on-change="zhuang_tai_select()"
+        >
           <!-- <Option value="quan_bu_zhuang_tai">全部状态</Option> -->
-          <Option value="dai_fu_kuan">代付款</Option>
-          <Option value="dai_qu_xiao">待取消</Option>
-          <Option value="yi_qu_xiao">已取消</Option>
-          <Option value="qu_xiao_b0_hui">取消驳回</Option>
-          <Option value="dai_pai_ke">代排课</Option>
-          <Option value="dai_que_ren">待确认</Option>
-          <Option value="dai_wan_jie">待完结</Option>
-          <Option value="tui_kuan_zhong">退款中</Option>
-          <Option value="yi_tui_kuan">已退款</Option>
-          <Option value="dai_ping_jia">待评价</Option>
-          <Option value="yi_wan_cheng">已完成</Option>
+          <Option value="0">代付款</Option>
+          <Option value="1">申请取消</Option>
+          <Option value="2">已取消</Option>
+          <Option value="3">取消驳回</Option>
+          <Option value="4">已付款</Option>
+          <Option value="6">申请退款</Option>
+          <Option value="7">退款中</Option>
+          <Option value="8">已退款</Option>
+          <Option value="9">支付失败</Option>
+          <Option value="13">待完结</Option>
+          <Option value="14">待评价</Option>
+          <Option value="15">结束订单</Option>
         </Select>
-        <Input type="text" placeholder="支付宝/手机账号/学员姓名" style="width:200px" />
-        <div style="margin-bottom:10px;">
+        <Input
+          type="text"
+          placeholder="学员姓名"
+          style="width: 200px"
+          @on-enter="searchenter"
+        />
+        <div style="margin-bottom: 10px">
           支付时间:
-          <DatePicker type="datetime" placeholder="请选择开始时间" style="width: 130px"></DatePicker>至:
-          <DatePicker type="datetime" placeholder="请选择结束时间" style="width: 130px"></DatePicker>
+          <DatePicker
+            type="datetime"
+            placeholder="请选择开始时间"
+            :confirm='false'
+            style="width: 130px"
+            on-change="DatePickerchange"
+          ></DatePicker
+          >至:
+          <DatePicker
+            type="datetime"
+            placeholder="请选择结束时间"
+            style="width: 130px"
+            :confirm='false'
+          ></DatePicker>
           <i-button type="primary">搜索</i-button>&emsp;
         </div>
       </div>
-      <tables ref="tables" v-model="tableData" :columns="columns" @on-delete="handleDelete" />
-      <div style="margin-top:20px">
+      <tables
+        ref="tables"
+        v-model="tableData"
+        :columns="columns"
+        @on-delete="handleDelete"
+      />
+      <div style="margin-top: 20px">
         <Page show-total :total="tableData.length" show-elevator></Page>
       </div>
       <!-- <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button> -->
@@ -42,7 +75,7 @@
 
 <script>
 import Tables from "_c/tables";
-import { getTableData } from "@/api/data";
+// import { getTableData } from "@/api/data";
 import untilMd5 from "../../../utils/md5";
 export default {
   name: "tables_page",
@@ -57,15 +90,27 @@ export default {
           width: 60,
           align: "center",
         },
-        { title: "支付时间", key: "name", sortable: false },
-        { title: "订单标题", key: "email", editable: false },
-        { title: "类型", key: "createTime" },
+        { title: "支付时间", key: "createTime", sortable: false },
+        { title: "订单标题", key: "trainCampName", editable: false },
+        { title: "类型", key: "type" },
         { title: "支付渠道", key: "createTime" },
         { title: "实收金额(元)", key: "money" },
         { title: "用户信息", key: "createTime" },
-        { title: "学员信息", key: "createTime" },
+        { title: "学员信息", key: "createUser" },
         { title: "教练信息", key: "createTime" },
-        { title: "联系信息", key: "createTime" },
+        {
+          title: "联系信息",
+          key: "contacts",
+          render: (h, params) => {
+            console.log(JSON.parse(params.row.contacts).name);
+            return h("div", [
+              h(
+                "span",
+                `${JSON.parse(params.row.contacts).name || "没有数据"}`
+              ),
+            ]);
+          },
+        },
         { title: "状态", key: "createTime" },
         {
           title: "操作",
@@ -119,36 +164,64 @@ export default {
         filename: `table-${new Date().valueOf()}.csv`,
       });
     },
-  },
-  mounted() {
-    this.axios
-      .post("/api/api/v2/business/trainOrder/getCoursePacketOrdersPage", {
-        orderType: 1,
-        // packetId: "",
-        // userId: "",
-        // studentId: "",
-        stauts: 1,
-        isDelete: 0,
+    getTrainCampOrdersPage(params) {
+      this.axios
+        .post("/api/api/v2/business/trainOrder/getTrainCampOrdersPage", {
+          ...params,
+          sign: untilMd5.toSign({ ...params }, "getTrainCampOrdersPage"),
+        })
+        .then((res) => {
+          console.log(res.data.data.list, "查询训练课包订单列表接口(分页)");
+          this.tableData = res.data.data.list;
+        });
+    },
+    lei_xingchange(e) {
+      console.log(this.lei_xing, "1111");
+      this.getTrainCampOrdersPage({
         pageNum: 1,
         pageSize: 10,
-        sign: untilMd5.toSign(
-          {
-            orderType: 1,
-            // packetId: "",
-            // userId: "",
-            // studentId: "",
-            stauts: 1,
-            isDelete: 0,
-            pageNum: 1,
-            pageSize: 10,
-          },
-          "getCoursePacketOrdersPage"
-        ),
-      })
-      .then((res) => {
-        console.log(res.data);
-        this.tableData = res.data.data.list;
+        orderType: this.lei_xing,
+        isDelete: 0,
+        // userId:JSON.parse(localStorage.getItem('user').id)
       });
+    },
+    zhuang_tai_select(){
+          this.getTrainCampOrdersPage({
+        stauts:this.zhuang_tai,
+        pageNum: 1,
+        pageSize: 10,
+        orderType: this.lei_xing,
+        isDelete: 0,
+        // userId:JSON.parse(localStorage.getItem('user').id)
+      });
+    },
+    searchenter(e){
+ console.log(e.target.value)
+ if(e.target.value){
+       this.getTrainCampOrdersPage({
+        studentName:e.target.value,
+        stauts:this.zhuang_tai,
+        pageNum: 1,
+        pageSize: 10,
+        orderType: this.lei_xing,
+        isDelete: 0,
+        // userId:JSON.parse(localStorage.getItem('user').id)
+      });
+ }
+    },
+    DatePickerchange(e){
+      console.log()
+    },
+
+  },
+  mounted() {
+    this.getTrainCampOrdersPage({
+      pageNum: 1,
+      pageSize: 10,
+      orderType: 1,
+      isDelete: 0,
+      // userId:JSON.parse(localStorage.getItem('user').id)
+    });
   },
 };
 </script>
