8885e7a90f0dff9a442e1582a675ca1afced737e
rgb613 <1920559968@qq.com>
2020-10-19 21:04:00 +0800

    [t0]修噶了首页的数据

--- src/components/charts/bar.vue
+++ src/components/charts/bar.vue
@@ -34,17 +34,101 @@ export default {
           subtext: this.subtext,
           x: 'center'
         },
+        	legend: {
+    			show:true,
+    			x:'center',
+    			y:'35',
+    			icon: 'stack',
+    			itemWidth:10,
+    			itemHeight:10,
+    			textStyle:{
+    				color:'#1bb4f6'
+    			},
+    			data:['已采纳','已发布']
+    		},
         xAxis: {
           type: 'category',
           data: xAxisData
         },
-        yAxis: {
-          type: 'value'
-        },
+        yAxis: [{
+            type: 'value',
+            name: '',
+            min: 0,
+            max: 200,
+            axisLabel: {
+                formatter: '{value}',
+                textStyle: {
+                    color: '#2ad1d2'
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                    color: '#27b4c2'
+                }
+            },
+            axisTick: {
+                show: false,
+            },
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    color: '#11366e'
+                }
+            }
+        },],
+        // seriesData
         series: [{
-          data: seriesData,
-          type: 'bar'
-        }]
+            name: '已采纳',
+            type: 'line',
+            stack: '总量',
+            symbol: 'circle',
+            // symbolSize: 8,
+            itemStyle: {
+                normal: {
+                    color: '#0092f6',
+                    lineStyle: {
+                        color: "#0092f6",
+                        width: 1
+                    },
+                    //           areaStyle: { 
+                    // 			//color: '#94C9EC'
+                    // 			color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+                    // 				offset: 0,
+                    // 				color: 'rgba(7,44,90,0.3)'
+                    // 			}, {
+                    // 				offset: 1,
+                    // 				color: 'rgba(0,146,246,0.9)'
+                    // 			}]),
+                    //           }
+                }
+            },
+            markPoint: {
+                itemStyle: {
+                    normal: {
+                        color: 'red'
+                    }
+                }
+            },
+            data: [10, 13, 101, 14, 90, 30, 10, 18]
+        },
+        {
+            name: '已发布',
+            type: 'line',
+            stack: '总量',
+            symbol: 'circle',
+            // symbolSize: 8,
+
+            itemStyle: {
+                normal: {
+                    color: '#00d4c7',
+                    lineStyle: {
+                        color: "#00d4c7",
+                        width: 1
+                    },
+                }
+            },
+            data: [142, 118, 44, 114, 29, 33, 131, 21]
+        },]
       }
       this.dom = echarts.init(this.$refs.dom, 'tdTheme')
       this.dom.setOption(option)
--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -18,19 +18,19 @@
     <Row :gutter="20" style="margin-top: 10px;">
       <i-col :md="24" :lg="8" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-pie style="height: 300px;" :value="pieData" text="用户访问来源"></chart-pie>
+          <chart-pie style="height: 300px;" :value="pieData" text="当日订单比例"></chart-pie>
         </Card>
       </i-col>
       <i-col :md="24" :lg="16" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-bar style="height: 300px;" :value="barData" text="每周用户活跃量" />
+          <chart-bar style="height: 300px;" :value="barData" text="近7日内订单" />
         </Card>
       </i-col>
     </Row>
     <Row>
-      <Card shadow>
+      <!-- <Card shadow>
         <example style="height: 310px;" />
-      </Card>
+      </Card> -->
     </Row>
   </div>
 </template>
@@ -39,6 +39,7 @@
 import InforCard from '_c/info-card'
 import CountTo from '_c/count-to'
 import { ChartPie, ChartBar } from '_c/charts'
+import untilMd5 from '../../../utils/md5'
 import Example from './example.vue'
 export default {
   name: 'home',
@@ -53,47 +54,64 @@ export default {
     return {
       inforCardData: [
         {
-          title: '新增用户',
+          title: '用户数据',
           icon: 'md-person-add',
           count: 803,
           color: '#2d8cf0'
         },
-        { title: '累计点击', icon: 'md-locate', count: 232, color: '#19be6b' },
+        { title: '用户数据', icon: 'md-locate', count: 232, color: '#19be6b' },
         {
-          title: '新增问答',
+          title: '用户数据',
           icon: 'md-help-circle',
           count: 142,
           color: '#ff9900'
         },
-        { title: '分享统计', icon: 'md-share', count: 657, color: '#ed3f14' },
+        { title: '用户数据', icon: 'md-share', count: 657, color: '#ed3f14' },
         {
-          title: '新增互动',
+          title: '用户数据',
           icon: 'md-chatbubbles',
           count: 12,
           color: '#E46CBB'
         },
-        { title: '新增页面', icon: 'md-map', count: 14, color: '#9A66E4' }
+        { title: '用户数据', icon: 'md-map', count: 14, color: '#9A66E4' }
       ],
       pieData: [
-        { value: 335, name: '直接访问' },
-        { value: 310, name: '邮件营销' },
-        { value: 234, name: '联盟广告' },
-        { value: 135, name: '视频广告' },
-        { value: 1548, name: '搜索引擎' }
+        { value: 335, name: '线上订单' },
+        { value: 310, name: '线下订单' },
+        // { value: 234, name: '联盟广告' },
+        // { value: 135, name: '视频广告' },
+        // { value: 1548, name: '搜索引擎' }
       ],
       barData: {
-        Mon: 13253,
-        Tue: 34235,
-        Wed: 26321,
-        Thu: 12340,
-        Fri: 24643,
-        Sat: 1322,
-        Sun: 1324
+        [`${new Date().getMonth()+1} 月 ${new Date().getDate()-6} 日`]: 13253,
+        [`${new Date().getMonth()+1} 月 ${new Date().getDate()-5} 日`]: 34235,
+       [`${new Date().getMonth()+1} 月 ${new Date().getDate()-4} 日`]: 26321,
+       [`${new Date().getMonth()+1} 月 ${new Date().getDate()-3} 日`]: 12340,
+        [`${new Date().getMonth()+1} 月 ${new Date().getDate()-2} 日`]: 24643,
+       [`${new Date().getMonth()+1} 月 ${new Date().getDate()-1} 日`]: 1322,
+       [`${new Date().getMonth()+1} 月 ${new Date().getDate()} 日`]: 1324
       }
     }
   },
+  methods:{
+     recommendList(params) {
+      this.axios
+        .post("/api/v2/data/iskate/recommendList", {
+          ...params,
+          sign: untilMd5.toSign({ ...params }, "recommendList"),
+        })
+        .then((res) => {
+          console.log(res.data, "首页(分页)");
+          this.tableData = res.data.data?res.data.data.list:[];
+        });
+    },
+  },
   mounted () {
-    //
+   this.recommendList({
+      homepageType: 1,
+      recommendType: 2,
+      // userId:JSON.parse(localStorage.getItem('user').id)
+    });
   }
 }
 </script>
