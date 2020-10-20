9565b32db32254145e659d910cd73052f243d31d
rgb613 <1920559968@qq.com>
2020-10-20 13:28:52 +0800

    [t0]运营管理的的表格和pagenum和pagesize以及删除逻辑的实现

--- src/view/yun_ying_guan_li/ke_bao_guan_li/ke_bao_guan_li_lie_biao.vue
+++ src/view/yun_ying_guan_li/ke_bao_guan_li/ke_bao_guan_li_lie_biao.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <Card>
-      <div style="margin-bottom:10px;">
+      <div style="margin-bottom: 10px">
         <i-button type="primary" @click="handlAdd">新增</i-button>&emsp;
         <i-button type="error" @click="handlRemove">删除</i-button>
       </div>
       <!-- editable 表格可编辑 -->
       <!-- searchable search-place="top" 搜索框-->
-      <tables ref="tables" v-model="tableData" :columns="columns" @on-delete="handleDelete" />
-      <div style="margin-top:20px">
-        <Page show-total :total="tableData.length" show-elevator></Page>
+      <tables
+        ref="tables"
+        v-model="tableData"
+        :columns="columns"
+        @on-delete="handleDelete"
+        @on-select-all="onselectall"
+        @on-select="onselect"
+        @on-select-cancel="onselectcancel"
+        @on-select-all-cancel="onselectallcancel"
+        @on-selection-change="onselectionchange"
+      />
+      <div style="margin-top: 20px">
+        <Page
+          show-total
+          @on-change="Pageonchange"
+          @on-page-size-change="onpagesizechange"
+          :total="total"
+          show-elevator
+        ></Page>
       </div>
-      <!-- <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button> -->
     </Card>
     <Xzkb v-if="xzkbModal" :rowInfo="row" :edit="edit" :onCancel="onCancel" />
   </div>
@@ -26,27 +41,40 @@ export default {
   name: "tables_page",
   components: {
     Tables,
-    Xzkb
+    Xzkb,
   },
   data() {
     return {
       xzkbModal: false,
-      row:{},
+      row: {},
       edit: false,
       columns: [
         {
           type: "selection",
           width: 60,
-          align: "center"
+          align: "center",
         },
         { title: "课包名称", key: "title", sortable: false },
-        { title: "课程安排", key: "email", editable: false },
-        { title: "应收(元)", key: "price" },
+        // { title: "课程安排", key: "email", editable: false },
+        { title: "应收(元)", key: "cost" },
         { title: "实收(元)", key: "specialPrice" },
-        { title: "状态", key: "status" },
+        {
+          title: "状态",
+          key: "status",
+          render: (h, params) => {
+            return h(
+              "span",
+              params.row.status === 0
+                ? "无效"
+                : params.row.status === 1
+                ? "有效"
+                : "暂停"
+            );
+          },
+        },
         { title: "关联门店", key: "rinkName" },
         { title: "创建者", key: "createUser" },
-        { title: "报名总数", key: "createTime" },
+        { title: "报名总数", key: "salesVolume" },
         {
           title: "操作",
           key: "action",
@@ -61,16 +89,16 @@ export default {
                   props: {
                     type: "primary",
 
-                    size: "small"
+                    size: "small",
                   },
                   style: {
-                    marginRight: "5px"
+                    marginRight: "5px",
                   },
                   on: {
                     click: () => {
                       this.look(params.row);
-                    }
-                  }
+                    },
+                  },
                 },
 
                 "查看"
@@ -82,27 +110,31 @@ export default {
                   props: {
                     type: "warning",
 
-                    size: "small"
+                    size: "small",
                   },
 
                   style: {
-                    marginRight: "5px"
+                    marginRight: "5px",
                   },
 
                   on: {
                     click: () => {
                       this.editBus(params.row);
-                    }
-                  }
+                    },
+                  },
                 },
 
                 "编辑"
-              )
+              ),
             ]);
-          }
-        }
+          },
+        },
       ],
-      tableData: []
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      deleteSelectIds: [],
     };
   },
   methods: {
@@ -111,17 +143,34 @@ export default {
       this.edit = false;
       this.xzkbModal = true;
     },
-    handlRemove(){
+    handlRemove() {
       this.$Modal.confirm({
-                    title: '提示',
-                    content: '<h3>确定删除该课包吗？</h3>',
-                    onOk: () => {
-                        this.$Message.info('删除成功');
-                    },
-                    onCancel: () => {
-                        // this.$Message.info('Clicked cancel');
-                    }
+        title: "提示",
+        content: "<h3>确定删除该课包吗？</h3>",
+        onOk: async () => {
+          let res = await this.deleteTableInId({
+            ids: this.deleteSelectIds.join(),
+            updateUser: JSON.parse(localStorage.user).id,
+          });
+          console.log(res.data)
+          if(res.data.code===200){
+           this.getList({
+                  isUseTemplate: 0,
+                  isMemberGoods: 0,
+                  status: 1,
+                  pageNum: this.pageNum,
+                  pageSize: this.pageSize,
                 });
+          this.$Message.info("删除成功");
+          }else{
+             this.$Message.info("删除失败");
+          }
+         
+        },
+        onCancel: () => {
+          this.$Message.info("取消删除");
+        },
+      });
     },
     onCancel() {
       this.xzkbModal = false;
@@ -141,39 +190,63 @@ export default {
     },
     exportExcel() {
       this.$refs.tables.exportCsv({
-        filename: `table-${new Date().valueOf()}.csv`
+        filename: `table-${new Date().valueOf()}.csv`,
       });
     },
-    getList() {
+    deleteTableInId(params) {
+      console.log(params);
+      return this.axios.post("/api/v2/data/course/batchDeleteCoursePacket", {
+        ...params,
+        sign: untilMd5.toSign({ ...params }, "batchDeleteCoursePacket"),
+      });
+    },
+    getList(params) {
       this.axios
         .post("/api/v2/data/course/getCoursePacketPage", {
-          // code: "xy-week-1",
-          isUseTemplate: 0,
-          isMemberGoods: 0,
-          status: 1,
-          pageNum: 1,
-          pageSize: 10,
-          sign: untilMd5.toSign(
-            {
-              // code: "xy-week-1",
-              isUseTemplate: 0,
-              isMemberGoods: 0,
-              status: 1,
-              pageNum: 1,
-              pageSize: 10
-            },
-            "getCoursePacketPage"
-          )
+          ...params,
+          sign: untilMd5.toSign({ ...params }, "getCoursePacketPage"),
         })
-        .then(res => {
-          console.log(res.data.data.list, "课包管理");
-          this.tableData = res.data.data.list;
+        .then((res) => {
+          console.log(res.data.data, "课包管理");
+          this.tableData = [...res.data.data.list];
+          this.total = res.data.data.total;
         });
-    }
+    },
+    Pageonchange(pageNum) {
+      this.getList({
+        isUseTemplate: 0,
+        isMemberGoods: 0,
+        status: 1,
+        pageNum: pageNum,
+        pageSize: this.pageSize,
+      });
+    },
+    onpagesizechange(pageNum) {
+      this.Pageonchange(pageNum);
+    },
+    onselect(select) {
+      console.log(select);
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
+    onselectall(selects) {},
+    onselectcancel(select) {
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
+    onselectallcancel(select) {},
+    onselectionchange(select) {
+      console.log(select, "11");
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
   },
   mounted() {
-    this.getList();
-  }
+    this.getList({
+      isUseTemplate: 0,
+      isMemberGoods: 0,
+      status: 1,
+      pageNum: this.pageNum,
+      pageSize: this.pageSize,
+    });
+  },
 };
 </script>
 
