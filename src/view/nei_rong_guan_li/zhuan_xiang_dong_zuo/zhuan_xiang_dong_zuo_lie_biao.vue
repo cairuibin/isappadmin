63f211e7718c5166c2d8b0adeca728ddb4e00880
rgb613 <1920559968@qq.com>
2020-10-20 22:24:08 +0800

    [t0]加入了专项动作的接口处理

--- src/view/nei_rong_guan_li/zhuan_xiang_dong_zuo/zhuan_xiang_dong_zuo_lie_biao.vue
+++ src/view/nei_rong_guan_li/zhuan_xiang_dong_zuo/zhuan_xiang_dong_zuo_lie_biao.vue
@@ -1,27 +1,41 @@
 <template>
   <div>
     <Card>
-      <div style="margin-bottom:10px;" class="header_wrap">
-
-         <i-button type="primary">新增</i-button>&emsp;
-          <i-button type="primary">删除</i-button>
+      <div style="margin-bottom: 10px" class="header_wrap">
+        <i-button @click="handlAdd" type="primary">新增</i-button>&emsp;
+        <i-button type="primary" @click="handlRemove">删除</i-button>
       </div>
-      <tables ref="tables" v-model="tableData" :columns="columns" @on-delete="handleDelete" />
-      <div style="margin-top:20px">
-        <Page show-total :total="tableData.length" show-elevator></Page>
+      <tables
+        ref="tables"
+        v-model="tableData"
+        :columns="columns"
+        @on-delete="handleDelete"
+        @on-select-all="onselectall"
+        @on-select="onselect"
+        @on-select-cancel="onselectcancel"
+        @on-select-all-cancel="onselectallcancel"
+        @on-selection-change="onselectionchange"
+      />
+      <div style="margin-top: 20px">
+        <Page
+          show-total
+          @on-change="Pageonchange"
+          @on-page-size-change="onpagesizechange"
+          :total="total"
+          show-elevator
+        ></Page>
       </div>
-      <!-- <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button> -->
-    </Card>
+      </Card>
   </div>
 </template>
 
 <script>
 import Tables from "_c/tables";
-import untilMd5 from '../../../utils/md5'
+import untilMd5 from "../../../utils/md5";
 export default {
   name: "tables_page",
   components: {
-      Tables
+    Tables,
   },
   data() {
     return {
@@ -32,11 +46,35 @@ export default {
           align: "center",
         },
         { title: "动作名称", key: "name", sortable: false },
-        { title: "动作配图", key: "email", editable: false },
-        { title: "标签", key: "createTime" },
-        { title: "状态", key: "createTime" },
-        { title: "排序", key: "createTime" },
-    
+        {
+          title: "动作配图",
+          key: "imageUrl",
+          render: (h, params) => {
+            return (
+              <div>
+                <img style={{ width: "30px" }} src={params.row.imageUrl} />
+              </div>
+            );
+          },
+        },
+        { title: "标签", key: "tag" },
+        {
+          title: "状态",
+          key: "status",
+          render: (h, params) => {
+            return (
+              <span>
+                {params.row.status === 0
+                  ? "无效"
+                  : params.row.status === 1
+                  ? "有效"
+                  : ""}
+              </span>
+            );
+          },
+        },
+        { title: "排序", key: "sortIndex" },
+
         {
           title: "操作",
 
@@ -68,7 +106,8 @@ export default {
                 },
 
                 "查看"
-              ), h(
+              ),
+              h(
                 "Button",
 
                 {
@@ -88,14 +127,18 @@ export default {
                 },
 
                 "编辑"
-              )
+              ),
             ]);
           },
         },
       ],
       tableData: [],
-      lei_xing:"全部类型",
-      zhuang_tai:"全部状态"
+      lei_xing: "全部类型",
+      zhuang_tai: "全部状态",
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      deleteSelectIds: [],
     };
   },
   methods: {
@@ -109,7 +152,7 @@ export default {
         filename: `table-${new Date().valueOf()}.csv`,
       });
     },
-       getTechniqueActionPage(params) {
+    getTechniqueActionPage(params) {
       this.axios
         .post("/api/v2/data/action/getTechniqueActionPage", {
           ...params,
@@ -120,17 +163,69 @@ export default {
           this.tableData = res.data.data.list;
         });
     },
+     deleteTableInId(params) {
+      console.log(params);
+      return this.axios.post("/api/v2/data/action/deleteTechniqueAction", {
+        ...params,
+        sign: untilMd5.toSign({ ...params }, "deleteTechniqueAction"),
+      });
+    },
+    Pageonchange(pageNum) {
+      this.getTechniqueActionPage({
+        pageNum: pageNum,
+        pageSize: this.pageSize,
+      });
+    },
+    onpagesizechange(pageNum) {
+      this.Pageonchange(pageNum);
+    },
+    onselect(select) {
+      console.log(select);
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
+    onselectall(selects) {},
+    onselectcancel(select) {
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
+    onselectallcancel(select) {},
+    onselectionchange(select) {
+      this.deleteSelectIds = select.map((v) => v.id);
+    },
+    handlRemove() {
+      this.$Modal.confirm({
+        title: "提示",
+        content: "<h3>确定删除该课包吗？</h3>",
+        onOk: async () => {
+          let res = await this.deleteTableInId({
+            ids: this.deleteSelectIds.join(),
+            updateUser: JSON.parse(localStorage.user).id,
+          });
+          console.log(res.data);
+          if (res.data.code === 200) {
+            this.getTechniqueActionPage({
+              pageNum: this.pageNum,
+              pageSize: this.pageSize,
+            });
+            this.$Message.info("删除成功");
+          } else {
+            this.$Message.info("删除失败");
+          }
+        },
+        onCancel: () => {
+          this.$Message.info("取消删除");
+        },
+      });
+    },
+    handlAdd() {},
   },
   mounted() {
-   this.getTechniqueActionPage({
-      pageNum: 1,
-      pageSize: 10,
-      
+    this.getTechniqueActionPage({
+      pageNum: this.pageNum,
+      pageSize: this.pageSize,
     });
   },
 };
 </script>
 
 <style scoped lang='scss'>
-
 </style>
